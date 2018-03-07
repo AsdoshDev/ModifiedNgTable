b038e50da46f34d8ffc52f494d8f5fea56b308fe
Gopalakrishnan <kg95298@nsroot.net>
2018-03-07 19:57:11 +0530

    pagination logic

--- src/app/shared/components/table/gsr-table-pagination/gsr-table-pagination.component.ts
+++ src/app/shared/components/table/gsr-table-pagination/gsr-table-pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'gsr-table-pagination',
@@ -10,47 +10,122 @@ export class GsrTablePaginationComponent implements OnInit {
 
   @Input() totalRecords;
   @Input() recordsPerPage;
-  numbers:any;
-  pages:any;
+  numbers: any;
+  pages: any;
   pageObj = [];
   constructor() {
 
-    
-   }
 
-  loadRecords(open,number){
-    debugger;
-    let targetObj = this.pageObj.findIndex(obj => obj.isClicked == 'active');
-      if(open == 'prev'){
-        if((targetObj) > 0){
-          this.loadRecords('',targetObj);       
-          return;
-        
-        }
-          return;
-        }
-      else if(open == 'next'){
-        if((targetObj + 2) < this.pageObj.length){
-          this.loadRecords('',targetObj + 2);
-          return;
-        }
-        return;
+  }
+
+  loadRecords(open, number) {
+    let targetPage = this.pageObj.find(obj => obj.isClicked == 'active');
+    let targetNumber = targetPage.number;
+    if (open == 'first') {
+      this.paginationInit();
+      return;
+    }
+    if (open == 'last') {
+      this.getLastnRecords();
+      return;
+    }
+    else if (open == 'prev') {
+      if ((targetNumber - 1) > 0) {
+        this.loadRecords('', targetNumber - 1);
       }
+      return;
+    }
+    else if (open == 'next') {
+      if ((targetNumber + 1) < this.pageObj[this.pageObj.length - 1].number) {
+        this.loadRecords('', targetNumber + 1);
+      }
+      return;
+    }
     this.pageObj.forEach(obj => obj.isClicked = "");
-    if(number){
+
+
+
+    if (number >= this.calcMean()) {
+      let lastNo = this.pageObj[this.pageObj.length - 1].number;
+      let diff = lastNo % number;
+      if (diff <= 3) {
+        let loopNo = (diff == 3) ? 1 : ((diff == 2) ? 2 : (diff == 1) ? 3 : (diff == 0) ? 4 : '');
+        let incrementNo = diff + 1;
+        for (let i = 0; i < loopNo; i++) {
+          if (this.pages >= number + incrementNo + i)
+            this.pageObj.push({ 'number': number + incrementNo + i, isClicked: '' });
+          if (this.pageObj.length > this.recordsPerPage)
+            this.pageObj.shift();
+
+        }
+      }
+    }
+    else if (number < this.calcMean() - 1) {
+      let firstNo = this.pageObj[0].number;
+      let diff;
+      if (number > firstNo)
+        diff = number % firstNo;
+      else
+        diff = firstNo % number;
+
+
+      if (diff <= 4) {
+        let loopNo = (diff == 0) ? 5 : ((diff == 2) ? 3 : (diff == 4) ? 1 : (diff == 3) ? 2 : diff == 1 ? 4 : '');
+        let decrementNo = 1;
+        for (let i = 0; i < loopNo; i++) {
+          if (firstNo - decrementNo - i > 0)
+            this.pageObj.unshift({ 'number': firstNo - decrementNo - i, isClicked: '' });
+          if (this.pageObj.length > this.recordsPerPage)
+            this.pageObj.pop();
+        }
+      }
+    }
+
+    if (number) {
       let targetObj = this.pageObj.find(obj => obj.number == number);
       targetObj.isClicked = 'active';
     }
-}
 
-  ngOnInit() {
-    this.pages = Math.ceil(this.totalRecords/this.recordsPerPage);
-    this.numbers =  Array.from(new Array(this.pages), (x,i) =>i+1);
+  }
+
+
+  calcMean() {
+    var total = 0;
+    for (var i = 0; i < this.recordsPerPage; i++) {
+      if (this.pageObj[i])
+        total += this.pageObj[i].number;
+    }
+    var avg = total / this.pageObj.length;
+    return Math.ceil(avg + 1);
+  }
+
+  getLastnRecords() {
+    this.pageObj = [];
+    this.pages = Math.ceil(this.totalRecords / this.recordsPerPage);
+    this.numbers = Array.from(new Array(this.pages), (x, i) => i + 1);
     var ctx = this;
-    this.numbers.map(function(number){
-      ctx.pageObj.push({'number': number,isClicked:''});
+    let tempArr = this.numbers.slice(-(this.recordsPerPage));
+    tempArr.map(function (number) {
+      ctx.pageObj.push({ 'number': number, isClicked: '' });
     });
     this.pageObj[0].isClicked = 'active';
+  }
+
+  paginationInit() {
+    this.pageObj = [];
+    this.pages = Math.ceil(this.totalRecords / this.recordsPerPage);
+    this.numbers = Array.from(new Array(this.pages), (x, i) => i + 1);
+
+    var ctx = this;
+    this.numbers.map(function (number) {
+      if (number <= ctx.recordsPerPage)
+        ctx.pageObj.push({ 'number': number, isClicked: '' });
+    });
+    this.pageObj[0].isClicked = 'active';
+  }
+
+  ngOnInit() {
+    this.paginationInit();
   }
 
 }
